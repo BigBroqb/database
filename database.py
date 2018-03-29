07547c700d2c3f3131cbf73a63dc3a91bfbfa617
Connor Thompson-Hebert <cthompsonheb@umass.edu>
2018-03-28 22:32:44 -0400

    added test user

--- database.py
+++ database.py
@@ -1,16 +1,18 @@
 from pymongo import MongoClient
 import datetime
 
+client = MongoClient("mongodb://bigbroqb:bigbrother@cluster0-shard-00-00-a4qe4.mongodb.net:27017,cluster0-shard-00-01-a4qe4.mongodb.net:27017,cluster0-shard-00-02-a4qe4.mongodb.net:27017/facebook?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
+db = client.facebook
+collection = db.users
+
+def add_new_user(user):
+	id = collection.insert_one(user).inserted_id
+	print(id)
+	return id
+
+def main(user):
+	add_new_user(user)
+
 if __name__ == "__main__":
-	client = MongoClient("mongodb://bigbroqb:bigbrother@cluster0-shard-00-00-a4qe4.mongodb.net:27017,cluster0-shard-00-01-a4qe4.mongodb.net:27017,cluster0-shard-00-02-a4qe4.mongodb.net:27017/facebook?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
-	db = client.facebook
-	collection = db.users
-
-	post = {"_id": 123456789, "author": "Joe", "text": "My first blog post!", "tags": ["mongodb", "python", "pymongo"], "date": datetime.datetime.utcnow()}
-
-	def add_new_user(user):
-		id = collection.insert_one(user).inserted_id
-		print(id)
-
-
-	add_new_user(post)
+	test_user = {"_id": 123456789, "name": "Connor Hebert", "address": "123 Maple Street", "age": 21,  "date": datetime.datetime.utcnow()}
+	main(test_user)
